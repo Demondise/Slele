c4bba12df4d390f112aa7ba996766f3744849879
shivam_shar <shivam_shar@LP-5CG0471MNB.HCLT.CORP.HCL.IN>
2024-03-17 11:01:46 +0530

    SHivam

---
+++ FrameworkDesign/src/main/java/SeleFrameworkDesign/FrameworkDesign/App.java
@@ -0,0 +1,13 @@
+package SeleFrameworkDesign.FrameworkDesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
